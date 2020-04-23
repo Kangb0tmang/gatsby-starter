98d8d3eca2a1fa1f4c7c2707748d1a9a6452dd2e
Jon Kang <jon@primemotive.com.au>
2020-04-23 20:34:02 +1000

    Vid #3: Creating pages

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Sweet beast licks your face so damn that dog jump off balcony, onto
+        stranger's head. Rub against owner because nose is wet hunt by meowing
+        loudly at 5am next to human slave food dispenser for play time. Instead
+        of drinking water from the cat bowl, make sure to steal water from the
+        toilet attack like a vicious monster for do i like standing on litter
+        cuz i sits when i have spaces, my cat buddies have no litter i live in
+        luxury cat life. If it fits i sits meow in empty rooms but scratch the
+        furniture bite off human's toes, yet meow meow, i tell my human and push
+        your water glass on the floor. Lick the other cats sleep nap and my
+        water bowl is clean and freshly replenished, so i'll drink from the
+        toilet and meow meow, i tell my human, yet catto munch salmono, inspect
+        anything brought into the house, sleep.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/company/team.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>Meet the Team</h1>
+      <p>
+        Chew master's slippers the fat cat sat on the mat bat away with paws and
+        going to catch the red dot today going to catch the red dot today ask to
+        be pet then attack owners hand be a nyan cat, feel great about it, be
+        annoying 24/7 poop rainbows in litter box all day i see a bird i stare
+        at it i meow at it i do a wiggle come here birdy. Howl uncontrollably
+        for no reason munch, munch, chomp, chomp so present belly, scratch hand
+        when stroked somehow manage to catch a bird but have no idea what to do
+        next, so play with it until it dies of shock.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
