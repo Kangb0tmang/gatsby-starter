e0a1d2711e313aad7ddca28142509b1aa17e9090
Jon Kang <jon@primemotive.com.au>
2020-04-29 14:12:10 +1000

    Vid #13: Page Query vs Static Query

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react"
+import { graphql } from 'gatsby'
 import Layout from "./layout"
+
+// Static query can be used anywhere
+// Doesn't accept variables, can't use context
+// Page query must be used on pages
 
 export default class postLayout extends Component {
   render() {
@@ -10,3 +15,20 @@ export default class postLayout extends Component {
     )
   }
 }
+
+export const query = graphql`
+  query PostQuery {
+    markdownRemark(frontmatter: {
+      slug: {
+        eq: "/third-post"
+      }
+    }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`
