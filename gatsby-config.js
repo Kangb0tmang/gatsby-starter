3937db8c818dfee4761d878c65ad875c1967f714
Jon Kang <jon@primemotive.com.au>
2020-04-30 19:38:14 +1000

    Vid #22: Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
 }
