bd393e05a7479dc4245ee06bdb998fde4356871e
Jon Kang <jon@primemotive.com.au>
2020-04-30 20:39:08 +1000

    Vid #23: Sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,11 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Kangb0tmang`,
+    siteUrl: `https://admiring-pike-d9ee74.netlify.app`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
